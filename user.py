dfd2ce0cff4a73b92784b2876b60a23c3c6639c0
Tyler Sheehan <143210854+TSheehan0127@users.noreply.github.com>
2025-03-01 14:04:39 -0500

    Add files via upload

--- user.py
+++ user.py
@@ -5,9 +5,8 @@ cursor = conn.cursor()
 
 class User:
 
-    def __init__(self, userID, username, password, name, email):
+    def __init__(self, userID, password, name, email):
         self.userID = userID
-        self.username = self.username
         self.password = self.password
         self.name = name
         self.email = email
@@ -16,7 +15,7 @@ class User:
     def from_db(cls, username, password):
         hash_pass = cls.hash(password)
         hash_user = cls.hash(username)
-        cursor.execute(f"SELECT * FROM users WHERE username = {hash_user}, password = {hash_pass}")
+        cursor.execute(f"SELECT * FROM users WHERE password_hash = {hash_pass}")
         data = cursor.fetchone()
         if data:
             return cls(data[0], data[1], data[2], f"{data[3]}  {data[4]}", data[5])
@@ -31,4 +30,7 @@ class User:
         return hashWord
 
     def retrieve_userID(cls):
-        id = cursor.execute(f"SELECT id FROM users WHERE username = {cls.username}")
+        id = cursor.execute(f"SELECT userID FROM user WHERE username_hash = {cls.username}")
+        
+    def add_user(cls):
+        cursor.execute(f"INSERT INTO user (password_hash, email, role) VALUES ({password}, {email}) ")
