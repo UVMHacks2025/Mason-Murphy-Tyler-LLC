0600ba04a50475b01314d1ea0f96e43e173507fe
tokeybear <masonritchie04@gmail.com>
2025-03-01 11:47:14 -0500

    user constructor from db added

--- user.py
+++ user.py
@@ -1,19 +1,27 @@
 import bcrypt
+import sqlite3
+conn = sqlite3.connect('riditDB.db')
+cursor = conn.cursor()
 
 class User:
+
     def __init__(self, username, password, name, email, phoneNumber):
-        self.username = username
-        self.password = self.hash_password(password)
+        self.username = hash(username)
+        self.password = self.hash(password)
         self.name = name
         self.email = email
         self.phoneNumber = phoneNumber
 
     @classmethod
     def from_db(cls, username, password):
+        hash_pass = cls.hash(password)
+        hash_user = cls.hash(username)
+        userData = cursor.execute(f"SELECT * FROM users WHERE username = {hash_user}, password = {hash_pass}")
+        return True
 
 
-    def hash_password(self, password):
-        byteWord = password.encode('UTF-8')
+    def hash(self, input):
+        byteWord = input.encode('UTF-8')
         salt = bcrypt.gensalt()
         hashWord = bcrypt.hashpw(byteWord, salt)
         return hashWord
